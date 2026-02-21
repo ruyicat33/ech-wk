a5a17a8ed11d35c067e2cc59a89eccd0b0ff2122
ruyicat33 <gdorc@protonmail.com>
2026-02-21 09:24:04 +0800

    Remove hardcoded token from _worker.js
    Remove hardcoded token value for security reasons.

--- _worker.js
+++ _worker.js
@@ -10,7 +10,7 @@ import { connect } from 'cloudflare:sockets';
 export default {
   async fetch(request, env, ctx) {
     try {
-      const token ='265dds';
+      const token ='';
       const upgradeHeader = request.headers.get('Upgrade');
       
       if (!upgradeHeader || upgradeHeader.toLowerCase() !== 'websocket') {
